8d403f890e93e14fe6a6af9cf783fa252bb34bcb
AbdulKhadarZilanKothakota <153972670+AbdulKhadarZilanKothakota@users.noreply.github.com>
2026-08-27 09:46:10 +0530

    Add files via upload

---
+++ JavaScript-Tasks/Task1/script.js
@@ -0,0 +1,37 @@
+// Create a variable for student name, age, and mark and print all three.
+
+let studentName = "Abdul";
+let studentAge = 25;
+let studentMarks = 85;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentMarks);
+
+
+// Ask the user's name using prompt() and display it using alert().
+
+let userName = prompt("Enter your name");
+
+alert("Your name is " + userName);
+
+
+// Ask the user's age using prompt() and print it using console.log().
+
+let userAge = prompt("Enter your age");
+
+console.log(userAge);
+
+
+// Ask the user a question using confirm().
+
+let question = confirm("Are you learning JavaScript?");
+
+console.log(question);
+
+
+// Ask the user's name and display it on the webpage using document.writeln().
+
+let user_Name = prompt("Enter your name");
+
+document.writeln("Your name is " + user_Name);
